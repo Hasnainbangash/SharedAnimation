e3f25a61a693e4ef9c0b45e3e813b90fb0897234
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 11:18:44 +0500

    PLACES DETAIL VIEW FILE CREATED ALONG WITH THE COMNENTS FOR THE STRUCTURE OF THE DESIGN AND THE CODE

---
+++ SharedAnimation/View/PlacesDetailView.swift
@@ -0,0 +1,30 @@
+//
+//  PlacesDetailView.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+struct PlacesDetailView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        // MARK: - IMAGE VIEW
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // MARK: - TITLE VIEW
+        
+        // MARK: - HIGHLIGHT SECTION VIEW
+        
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    PlacesDetailView()
+}
