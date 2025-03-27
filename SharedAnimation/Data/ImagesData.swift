6f98184d37b3c333d2800ee630ada7fd87e60309
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 10:07:37 +0500

    Images model and data work done

---
+++ SharedAnimation/Data/ImagesData.swift
@@ -0,0 +1,25 @@
+//
+//  ImagesData.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+// MARK: - IMAGES DATA
+
+let imagesData: [ImagesModel] = [
+    ImagesModel(imageName: "hotelroomimage1"),
+    ImagesModel(imageName: "hotelroomimage2"),
+    ImagesModel(imageName: "hotelroomimage3"),
+    ImagesModel(imageName: "hotelroomimage4"),
+    ImagesModel(imageName: "hotelroomimage5"),
+    ImagesModel(imageName: "hotelroomimage6"),
+    ImagesModel(imageName: "hotelroomimage7"),
+    ImagesModel(imageName: "hotelroomimage8"),
+    ImagesModel(imageName: "hotelroomimage9"),
+    ImagesModel(imageName: "hotelroomimage10"),
+    ImagesModel(imageName: "hotelroomimage11"),
+    ImagesModel(imageName: "hotelroomimage12")
+]
---
+++ SharedAnimation/Model/ImagesModel.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesModel.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+// MARK: - IMAGES MODEL
+
+struct ImagesModel: Identifiable {
+    let id = UUID()
+    let imageName: String
+}
