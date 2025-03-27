ffa652eacd4653d22a63f2bcbeb93601f4266dc3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 10:02:17 +0500

    Assests are added to the project in form of images and uses the code structure comments in the home4 images view

---
+++ SharedAnimation/View/HomeImagesView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeImagesView.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+struct HomeImagesView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    HomeImagesView()
+}
