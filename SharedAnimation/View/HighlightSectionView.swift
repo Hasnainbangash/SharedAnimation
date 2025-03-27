95d26a66c4370de0085bc6d0b18f14ed96f102a2
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 12:12:17 +0500

    Highlight section view design work done

--- SharedAnimation/View/HighlightSectionView.swift
+++ SharedAnimation/View/HighlightSectionView.swift
@@ -10,15 +10,36 @@ import SwiftUI
 struct HighlightSectionView: View {
     // MARK: - PROPERTIES
     
+    var imageName: String
+    var title: String
+    var titleDescription: String
+    
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top, spacing: 12) {
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .offset(y: 7)
+                .frame(width: 30, height: 30)
+                .foregroundColor(Color.pink)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(title)
+                    .font(.system(size: 18, weight: .semibold, design: .rounded))
+                    .foregroundColor(Color.black)
+                
+                Text(titleDescription)
+                    .font(.system(size: 16, design: .rounded))
+                    .foregroundColor(Color.gray)
+            } //: VSTACK
+        } //: HSTACK
     }
 }
 
 // MARK: - PREVIEW
 
 #Preview {
-    HighlightSectionView()
+    HighlightSectionView(imageName: "suitcase", title: "Only 13 hours left to book", titleDescription: "The host will stop accepting bookings for your dates soon.")
 }
