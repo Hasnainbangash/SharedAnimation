2570fa9961e34cde81814112d2be5465a0b054c4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 09:49:35 +0500

    Initial Commit

---
+++ SharedAnimation/SharedAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedAnimationApp.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SharedAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
