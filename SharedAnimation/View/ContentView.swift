972366a3a569cf427b6c4c95d8ab02504450996a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 12:50:33 +0500

    The detail view is presentaed by using the full cover view work done

--- SharedAnimation/View/ContentView.swift
+++ SharedAnimation/View/ContentView.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var isDetailViewPresented: Bool = false
+    
+    // MARK: - BODY
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(1...5, id: \.self) { _ in
                     HomeView()
                         .listRowInsets(EdgeInsets())
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                self.isDetailViewPresented.toggle()
+                            }
+                        }
                 } //: LOOP
             } //: LIST
             .listStyle(PlainListStyle())
             .navigationBarTitle("Airbnb", displayMode: .large)
+            .fullScreenCover(isPresented: $isDetailViewPresented) {
+                PlacesDetailView()
+            }
         } //: NAVIGATION
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     ContentView()
