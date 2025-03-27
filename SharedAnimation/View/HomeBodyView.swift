fa65ff341b0acd211f5629c57ae4c5aa2fd9f526
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 10:18:25 +0500

    Home body view file is created along with the comments for the bod striture is done

---
+++ SharedAnimation/View/HomeBodyView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeBodyView.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+struct HomeBodyView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    HomeBodyView()
+}
