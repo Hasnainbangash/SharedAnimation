981bf93c81d63d3689a2d14b1a2c464aeed0766c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 11:58:19 +0500

    Hight light section vie file is added to the project along with the comments are added to the project

---
+++ SharedAnimation/View/HighlightSectionView.swift
@@ -0,0 +1,24 @@
+//
+//  HighlightSectionView.swift
+//  SharedAnimation
+//
+//  Created by Elexoft on 27/03/2025.
+//
+
+import SwiftUI
+
+struct HighlightSectionView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    HighlightSectionView()
+}
