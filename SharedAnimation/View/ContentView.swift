9531e44592bcbaf6b70f512ddb6bd2c8b27ccd65
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-27 14:35:51 +0500

    Some random colors are to the content view fr the home view

--- SharedAnimation/View/ContentView.swift
+++ SharedAnimation/View/ContentView.swift
@@ -20,6 +20,11 @@ struct ContentView: View {
                 ForEach(1...5, id: \.self) { _ in
                     HomeView()
                         .listRowInsets(EdgeInsets())
+                        .background(Color(
+                            red: Double.random(in: 0...1),
+                            green: Double.random(in: 0...1),
+                            blue: Double.random(in: 0...1)
+                        ))
                         .onTapGesture {
                             withAnimation(.spring()) {
                                 self.isDetailViewPresented.toggle()
